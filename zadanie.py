85dc5736cde13e4570fcecfd6281b6aea9ee5fbc
Miłosz Książek <s478087@pc23h.labs.wmi.amu.edu.pl>
2022-01-10 11:07:08 +0100

    first commit

---
+++ zadanie.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Wymyśl swoją (dowolną, ale indywidualną, inną od pozostałych osób) serię obliczeń (mnożenie, dzielenie, odejmowanie, dodawanie, potęgowanie, inne działania dla liczb parzystych i nieparzystych...) na ciągu liczb A i liczb B, tak , żeby w funkcji zajęło to minimum 3 linijki kodu i żeby wynik był pojedyńczą liczbą "wynik"
+# funkcje dla drugiej osoby z grupy pozostaw pustą
+# Do funkcji "obliczenia_wspolne" dodaj swoje rozwiązanie
+
+def obliczenia_osoby_A(liczbyA, liczbyB):
+    wynik = 0
+    return wynik
+def obliczenia_osoby_B(liczbyA, liczbyB):
+    wynik = 0
+    return wynik
+#def obliczenia_osoby_C(liczbyA, liczbyB):
+#    wynik = 0
+#    return wynik
+    
+def obliczenia_wspolne(liczbyA, liczbyB):
+    wynik = 0
+    return wynik
+
+A = [9,2,1,4,1,4,3,5,2,5,36,78,6,865,4,5,746,46,3,5]
+
+B = [8,4,645,6,64,6,456,6,56,6,7,57,547,4,7457,58,4,85]
+
+print("wynik obliczenia_osoby A:")
+print(obliczenia_osoby_A(A, B))
+
+print("wynik obliczenia_osoby B:")
+print(obliczenia_osoby_B(A, B))
+
+#print("wynik obliczenia_osoby C:")
+#print(obliczenia_osoby_C(A, B))
+
+print("wynik obliczenia_wspolne:")
+print(obliczenia_wspolne(A, B))
